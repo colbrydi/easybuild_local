ca33cade8c03173239b0803f7c719a0120771288
Kyllan Wunder <fencer4933@gmail.com>
2024-08-29 09:20:13 -0400

    added useful follow up commands

--- eb_local_reset.sh
+++ eb_local_reset.sh
@@ -5,3 +5,8 @@
 rm -rf software
 mkdir -p ./software/modules/
 mkdir -p ./tempdir/
+
+
+echo "Deleted and reset install directory. Suggested commands to run next:"
+echo "conda deactivate"
+echo "module purge"
