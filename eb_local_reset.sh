25dce48cab93bc67aa4daab2cc22d470d84b1176
Kyllan Wunder <fencer4933@gmail.com>
2024-08-30 14:07:23 -0400

    adding unuse to reset modules

--- eb_local_reset.sh
+++ eb_local_reset.sh
@@ -2,10 +2,11 @@
 
 # Deletes and reset's install directory
 
-rm -rf software
+rm -rf software tempdir
 mkdir -p ./software/modules/
 mkdir -p ./tempdir/
 
+module unuse ./software/modules/all
 
 echo "Deleted and reset install directory. Suggested commands to run next:"
 echo "conda deactivate"
